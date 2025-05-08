4f829843835552f3f1766cd3450c0e52b94373be
anAhmed02 <Ahmednurahmed02@gmail.com>
2025-05-08 18:14:32 +0200

    satt opp nav

--- billettlyst/src/App.jsx
+++ billettlyst/src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
 import Home from "./komponenter/Home";
 import EventPage from "./komponenter/EventPage";
 import CategoryPage from "./komponenter/CategoryPage";
@@ -6,12 +8,15 @@ import Dashboard from "./komponenter/Dashboard";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" elemen={<Home />} />
-            <Route path="/event/:id" elemnt={<EventPage />} />
-            <Route path="/category/:slug" elemnt={<CategoryPage />} />
-            <Route path="/dashboard" elemnt={<Dashboard />} />
-        </Routes>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" elemen={<Home />} />
+                <Route path="/event/:id" elemnt={<EventPage />} />
+                <Route path="/category/:slug" elemnt={<CategoryPage />} />
+                <Route path="/dashboard" elemnt={<Dashboard />} />
+            </Routes>
+        </Router>
     );
 }
 
---
+++ billettlyst/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import {link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <nav>
+            <link to="/">Billetlyst</link>
+            <link to="/category/musikk">Musikk</link>
+            <link to="/category/musikk">Musikk</link>
+            <link to="/category/musikk">Teater</link>
+            <link to="/dashboard">Logg inn</link>
+        </nav>
+    );
+}
+
+
+export default Navbar;
