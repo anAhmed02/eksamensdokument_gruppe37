09a681ea9ebd1296bf14691c7cb82c1b4569cd78
anAhmed02 <Ahmednurahmed02@gmail.com>
2025-05-09 15:38:39 +0200

    oppdaterte home
    la til apiet som henter de forskjellige festivalene

--- billettlyst/src/komponenter/Home.jsx
+++ billettlyst/src/komponenter/Home.jsx
@@ -2,7 +2,44 @@ import { useEffect, useState } from React;
 import { Link } from "react-router-dom";
 
 function Home() {
+    const [festivals, setFestivals] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
 
+    const API_KEY = "HX1eiAg74CfyBUI7OUbj9WGgGxkGWJDs"; 
+
+    const festivalNames = [
+    "Findings",
+    "Neon",
+    "Skeikampenfestivalen",
+    "Tons of Rock"
+    ];
+
+    useEffect(() => {
+        async function fetchFestivals() {
+            const results = await Promise.all(
+                festivalNames.map(async (name) => {
+                    const res = await fetch(
+                        `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${encodeURIComponent(
+                        name
+                    )}&countryCode=NO&apikey=${API_KEY}`
+                );
+                const data = await res.json();
+                return data._embedded?.events?.[0]; 
+            })
+      );
+      setFestivals(results.filter(Boolean));
+    }
+
+    fetchFestivals();
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      navigate(`/category/${searchTerm.toLowerCase()}`);
+    }
+  };
 
     return (
         <main>
