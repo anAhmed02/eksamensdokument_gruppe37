80617afcc10771a36da51113c30b46b91da5bbb5
anAhmed02 <Ahmednurahmed02@gmail.com>
2025-05-12 17:47:34 +0200

    satt opp react og satt inn app.jsx

---
+++ billettlyst/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './komponenter/Navbar';
+import Home from "./komponenter/Home";
+import EventPage from "./komponenter/EventPage";
+import CategoryPage from "./komponenter/CategoryPage";
+import Dashboard from "./komponenter/Dashboard";
+
+function App() {
+    
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/event/:id" element={<EventPage />} />
+                <Route path="/category/:slug" element={<CategoryPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
